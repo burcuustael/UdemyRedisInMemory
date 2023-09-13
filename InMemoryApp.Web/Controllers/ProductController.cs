e53b595bb6e18fbef1db91eee925b1589bfa2211
burcuustael <burcustael@gmail.com>
2023-09-13 19:06:17 +0300

    Add project files.

---
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemoryApp.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IMemoryCache _memoryCache;
+        public ProductController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+        public IActionResult Index()
+        {
+            
+            MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
+
+            options.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+            options.SlidingExpiration = TimeSpan.FromSeconds(10);
+            options.Priority = CacheItemPriority.High;
+
+            options.RegisterPostEvictionCallback((key, value, reason, state) =>
+            {
+                _memoryCache.Set("callback", $"{key}-->{value} => sebep:{reason}");
+
+            });
+
+            _memoryCache.Set<string>("zaman", DateTime.Now.ToString(), options);
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            _memoryCache.TryGetValue("zaman", out string zamanache);
+
+            ViewBag.zaman = zamanache;
+
+            return View();
+        }
+    }
+}
